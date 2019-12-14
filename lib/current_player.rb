08eda1cea84ee94d5285e4ef0ee335f45f92ea32
Will Carter <will.carter@fergusllc.com>
2019-12-14 00:31:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,16 @@
 def turn_count(board)
-  board.each { |space|
-    puts space
+  count = 0
+  board.each { |position|
+    if position != " "
+      count += 1
+    end
   }
+  count
 end
 
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+ 
 def current_player
   
 end
